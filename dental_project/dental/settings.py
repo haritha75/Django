de0388987325eb255a0d3c8eddef197f4071c5e3
Haritha <yerukonduharitha@gmail.com>
2024-04-19 09:44:29 +0530

    Dental project updated

--- dental_project/dental/settings.py
+++ dental_project/dental/settings.py
@@ -126,3 +126,19 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# EMAIL_HOST = 'smtp.google.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'drjohn@gmail.com'
+# EMAIL_HOST_PASSWORD = 'drpassword'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
+# sending mail using localhost
+# python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT =  1025
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_TLS = False
